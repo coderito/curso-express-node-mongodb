441942637b1ed51556e3fcd4dd03df6575db62e6
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-03-22 16:18:13 -0300

    Creando model y Schema

---
+++ 2-Proyecto-node/models/Url.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { nanoid } from 'nanoid'
+const { Schema } = mongoose;
+
+const urlSchema = new Schema({
+    origin: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    shortURL: {
+        type: String,
+        unique: true,
+        required: true,
+        default: nanoid(6)
+    }
+})
+
+const Url = mongoose.model("Url", urlSchema);
+module.exports = Url;
